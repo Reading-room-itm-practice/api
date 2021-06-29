7ee53b39851fb8bdb06056e4f087dbc3fd08cdc9
Justyn Chroł <57270554+Justyn-98@users.noreply.github.com>
2021-06-29 10:57:44 +0200

    #1 ef installed & docker configured (#18)

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI.Installers;
 
 namespace WebAPI
 {
@@ -23,18 +24,11 @@ namespace WebAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
-            });
+            services.InstallServicesInAssembly(Configuration);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
