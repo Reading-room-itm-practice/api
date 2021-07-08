aec01d5dff169af2299f0b0de0dd1dc970675a88
Justyn Chrol <justyn.chr1@gmail.com>
2021-07-08 10:26:25 +0200

    #35 created dependency injection

--- Core/DependencyInjection.cs
+++ Core/DependencyInjection.cs
@@ -1,27 +1,34 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Core.Interfaces;
+using Core.Repositories;
+using Core.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Storage.Interfaces;
+using Storage.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
-using Core.Interfaces;
-using Core.Services;
-using WebAPI.Services;
-using Storage.Interfaces;
-using Storage.Models;
 
-namespace WebAPI.Installers
+namespace Core
 {
-    public class ServiceInstaller : Installer
+    public static class DependencyInjection
     {
-        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddCore(this IServiceCollection services)
         {
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IBaseRepository<Author>, BaseRepository<Author>>();
+
             services.AddScoped<ICategoryService, CategoryService>();
-            services.AddSingleton<ILoggedUserProvider, LoggedUserProvider>();
             services.AddScoped<ICreatorService<Author>, CreatorService<Author>>();
             services.AddScoped<IGetterService<Author>, GetterService<Author>>();
             services.AddScoped<IUpdaterService<Author>, UpdaterService<Author>>();
             services.AddScoped<IDeleterService<Author>, DeleterService<Author>>();
+
+            return services;
         }
     }
 }
