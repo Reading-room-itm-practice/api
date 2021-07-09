dd61eed74278c64edb84ddf65a32d210598976a0
B34TER <35150621+B34TER@users.noreply.github.com>
2021-07-09 10:10:49 +0200

    #45 Applied changes to collective CRUD service (#46)
    * #45 Changes completed
    
    * #45 Added line between properties and constructors

--- Core/DependencyInjection.cs
+++ Core/DependencyInjection.cs
@@ -20,11 +20,27 @@ namespace Core
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             services.AddScoped<IBaseRepository<Author>, BaseRepository<Author>>();
+            services.AddScoped<IBaseRepository<Book>, BaseRepository<Book>>();
+            services.AddScoped<IBaseRepository<Category>, BaseRepository<Category>>();
+
+            services.AddScoped<ICrudService<Author>, CrudService<Author>>();
+            services.AddScoped<ICrudService<Book>, CrudService<Book>>();
+            services.AddScoped<ICrudService<Category>, CrudService<Category>>();
 
             services.AddScoped<ICreatorService<Author>, CreatorService<Author>>();
             services.AddScoped<IGetterService<Author>, GetterService<Author>>();
             services.AddScoped<IUpdaterService<Author>, UpdaterService<Author>>();
             services.AddScoped<IDeleterService<Author>, DeleterService<Author>>();
+
+            services.AddScoped<ICreatorService<Book>, CreatorService<Book>>();
+            services.AddScoped<IGetterService<Book>, GetterService<Book>>();
+            services.AddScoped<IUpdaterService<Book>, UpdaterService<Book>>();
+            services.AddScoped<IDeleterService<Book>, DeleterService<Book>>();
+
+            services.AddScoped<ICreatorService<Category>, CreatorService<Category>>();
+            services.AddScoped<IGetterService<Category>, GetterService<Category>>();
+            services.AddScoped<IUpdaterService<Category>, UpdaterService<Category>>();
+            services.AddScoped<IDeleterService<Category>, DeleterService<Category>>();
 
             return services;
         }
