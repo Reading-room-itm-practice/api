fa4f085cdfe7b02659a070e227b518c06dc6eacd
pvc <pvc@DESKTOP-P1FVAP0>
2021-07-13 13:56:46 +0200

    #7 updated ISearchService

--- Core/Interfaces/ISearchService.cs
+++ Core/Interfaces/ISearchService.cs
@@ -14,6 +14,7 @@ namespace Core.Interfaces
     {
         public ServiceResponse SearchCategory(string searchString, SortType? sort);
         public ServiceResponse SearchBook(string searchString, SortType? sort, int? minYear, int? maxYear, int? categoryId);
+        public ServiceResponse SearchBook(string searchString, SortType? sort);
         public ServiceResponse SearchAuthor(string searchString, SortType? sort);
         public ServiceResponse SearchUser(string searchString, SortType? sort);
         public ServiceResponse SearchAll(string searchString, SortType? sort);
