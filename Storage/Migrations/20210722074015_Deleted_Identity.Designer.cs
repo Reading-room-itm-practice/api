e87798da305b08cafaed1ec7bfaeeab351594d09
Justyn Chrol <justyn.chr1@gmail.com>
2021-07-22 09:41:40 +0200

    #66 created migration deleting identity

--- Storage/Migrations/20210722074015_Deleted_Identity.Designer.cs
+++ Storage/Migrations/20210722074015_Deleted_Identity.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Storage.DataAccessLayer;
 
 namespace Storage.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210722074015_Deleted_Identity")]
+    partial class Deleted_Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
