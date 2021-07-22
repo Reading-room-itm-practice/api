206726ffad81b5f655e22843dfef87b0d0380eb7
Justyn Chrol <justyn.chr1@gmail.com>
2021-07-22 09:26:06 +0200

    #66 changed book relase date type

--- Storage/Models/Book.cs
+++ Storage/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using Storage.Iterfaces;
 using Storage.Models.Photos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -11,15 +12,15 @@ namespace Storage.Models
     {
         [Key]
         public int Id { get; set; }
-        public int AuthorId { get; set; }
+        public int? AuthorId { get; set; }
         public Author Author { get; set; }
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
         public Category Category { get; set; }
-        public int MainPhotoId { get; set; }
+        public int? MainPhotoId { get; set; }
         public Photo MainPhoto { get; set; }
         public ICollection<BookPhoto> Photos { get; set; }
         public string Title { get; set; }
-        public int ReleaseYear { get; set; }
+        public DateTime? RelaseDate { get; set; }
         public string Description { get; set; }
         public ICollection<ReadStatus> ReadStatuses { get; set; }
         public ICollection<Review> Reviews { get; set; }
