a30a82944e896e3a2d15dc6d329de433a3281ef2
sew.szymanski@gmail.com <sew.szymanski@gmail.com>
2021-07-28 09:20:03 +0200

    #11 Syntax sugar fix

--- Core/Services/UserCrudService.cs
+++ Core/Services/UserCrudService.cs
@@ -32,7 +32,7 @@ namespace Core.Services
 
         public override async Task<ServiceResponse<IEnumerable<IDto>>> GetAll<IDto>()
         {
-            var models = await _repository.FindByConditions(x => x.Approved == true);
+            var models = await _repository.FindByConditions(x => x.Approved);
             var responseModels = _mapper.Map<IEnumerable<IDto>>(models);
 
             return ServiceResponse<IEnumerable<IDto>>.Success(responseModels, "Retrieved list with resorces");
@@ -40,7 +40,7 @@ namespace Core.Services
 
         public override async Task<ServiceResponse<IDto>> GetById<IDto>(int id)
         {
-            var model = await _repository.FindByConditions(x => x.Approved == true && x.Id == id);
+            var model = await _repository.FindByConditions(x => x.Approved && x.Id == id);
             var responseModel = _mapper.Map<IDto>(model.FirstOrDefault());
 
             return ServiceResponse<IDto>.Success(responseModel, "Retrieved resource");
