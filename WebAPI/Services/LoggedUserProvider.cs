e7137b92804e9ca3eabaf31c6a0476b4a8dfe048
mariuszskuza02@gmail.com <mariuszskuza02@gmail.com>
2021-08-03 09:42:00 +0200

    #61 security

--- WebAPI/Services/LoggedUserProvider.cs
+++ WebAPI/Services/LoggedUserProvider.cs
@@ -16,7 +16,7 @@ namespace WebAPI.Services
 
         public Guid GetUserId()
         {
-            var loggedUserId = "5ac2cfbf-b8a6-43d9-c1c8-08d9564ae8f1";//_httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            var loggedUserId = _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
 
             return loggedUserId != null ? new Guid(loggedUserId) : throw new UnauthorizedAccessException();
         }
