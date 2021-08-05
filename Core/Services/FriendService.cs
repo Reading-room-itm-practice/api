76986d8ac75b0e3b64ced1bf479645cf0261ffb6
sew.szymanski@gmail.com <sew.szymanski@gmail.com>
2021-08-05 14:55:18 +0200

    #13 Removed some conditions from Friend Service, so code is cleaner. Fixed bug.

--- Core/Services/FriendService.cs
+++ Core/Services/FriendService.cs
@@ -18,7 +18,7 @@ namespace Core.Services
 {
     public class FriendService : CrudService<FriendRequest>, IFriendService
     {
-        private readonly IFriendRepository _repository;
+        private readonly IFriendRepository _friendRepository;
 
         private readonly ILoggedUserProvider _loggedUser;
         private readonly IMapper _mapper;
@@ -32,7 +32,7 @@ namespace Core.Services
             IDeleterService<FriendRequest> deleter)
             : base(creator, getter, updater, deleter)
         {
-            _repository = repository;
+            _friendRepository = repository;
             _mapper = mapper;
             _loggedUser = loggedUser;
         }
@@ -40,42 +40,36 @@ namespace Core.Services
         public async Task<ServiceResponse<IEnumerable<FriendDto>>> GetApprovedFriendRequests(Guid? userId)
         {
             var searchedUserId = userId ?? _loggedUser.GetUserId();
-            var friendRequests = await _repository.GetSentAndReceivedFriendRequests(searchedUserId);
+            var friendRequests = await _friendRepository.GetSentAndReceivedFriendRequests(searchedUserId);
             var friendList = new List<FriendDto>();
 
             foreach (var friendRequest in friendRequests)
             {
-                var user = friendRequest.ToId == userId ? friendRequest.Creator : friendRequest.To;
+                var user = friendRequest.ToId == userId ? friendRequest.To : friendRequest.Creator;
                 friendList.Add(new FriendDto { Id = user.Id, UserName = user.UserName });
             }
-            
+
             return ServiceResponse<IEnumerable<FriendDto>>.Success(friendList, "Retrieved list with resources.");
         }
 
         public async Task<ServiceResponse<IEnumerable<SentFriendRequestDto>>> GetSentFriendRequests(int? friendRequestId)
         {
             Guid userId = _loggedUser.GetUserId();
-            var friendRequests = await _repository.GetSentFriendRequests(userId);
+            var friendRequests = await _friendRepository.GetSentFriendRequests(userId);
             var friendList = new List<SentFriendRequestDto>();
 
-            if (friendRequests.Any())
+            var foundFriendRequest = friendRequests.FirstOrDefault(fr => fr.Id == friendRequestId);
+            if (foundFriendRequest != null)
             {
-                if (friendRequestId != null)
+                var user = foundFriendRequest.To;
+                friendList.Add(new SentFriendRequestDto { Id = foundFriendRequest.Id, ToId = user.Id, UserName = user.UserName });
+            }
+            else if (friendRequestId == null)
+            {
+                foreach (var friendRequest in friendRequests)
                 {
-                    var friendRequest = _mapper.Map<FriendRequest>(friendRequests.Where(fr => fr.Id == friendRequestId).FirstOrDefault());
-                    if (friendRequest != null)
-                    {
-                        var user = friendRequest.To;
-                        friendList.Add(new SentFriendRequestDto { Id = friendRequest.Id, ToId = user.Id, UserName = user.UserName });
-                    }
-                }
-                else
-                {
-                    foreach (var friendRequest in friendRequests)
-                    {
-                        var user = friendRequest.ToId == userId ? friendRequest.Creator : friendRequest.To;
-                        friendList.Add(new SentFriendRequestDto { Id = friendRequest.Id, ToId = user.Id, UserName = user.UserName });
-                    }
+                    var user = friendRequest.ToId == userId ? friendRequest.Creator : friendRequest.To;
+                    friendList.Add(new SentFriendRequestDto { Id = friendRequest.Id, ToId = user.Id, UserName = user.UserName });
                 }
             }
 
@@ -85,27 +79,21 @@ namespace Core.Services
         public async Task<ServiceResponse<IEnumerable<ReceivedFriendRequestDto>>> GetReceivedFriendRequests(int? friendRequestId)
         {
             Guid userId = _loggedUser.GetUserId();
-            var friendRequests = await _repository.GetReceivedFriendRequests(userId);
+            var friendRequests = await _friendRepository.GetReceivedFriendRequests(userId);
             var friendList = new List<ReceivedFriendRequestDto>();
 
-            if (friendRequests.Any())
+            var foundFriendRequest = friendRequests.FirstOrDefault(fr => fr.Id == friendRequestId);
+            if (foundFriendRequest != null)
             {
-                if (friendRequestId != null)
+                var user = foundFriendRequest.Creator;
+                friendList.Add(new ReceivedFriendRequestDto { Id = foundFriendRequest.Id, CreatorId = user.Id, UserName = user.UserName });
+            }
+            else if (friendRequestId == null)
+            {
+                foreach (var friendRequest in friendRequests)
                 {
-                    var friendRequest = _mapper.Map<FriendRequest>(friendRequests.Where(fr => fr.Id == friendRequestId).FirstOrDefault());
-                    if (friendRequest != null)
-                    {
-                        var user = friendRequest.Creator;
-                        friendList.Add(new ReceivedFriendRequestDto { Id = friendRequest.Id, CreatorId = user.Id, UserName = user.UserName });
-                    }
-                }
-                else
-                {
-                    foreach (var friendRequest in friendRequests)
-                    {
-                        var user = friendRequest.ToId == userId ? friendRequest.Creator : friendRequest.To;
-                        friendList.Add(new ReceivedFriendRequestDto { Id = friendRequest.Id, CreatorId = user.Id, UserName = user.UserName });
-                    }
+                    var user = friendRequest.ToId == userId ? friendRequest.Creator : friendRequest.To;
+                    friendList.Add(new ReceivedFriendRequestDto { Id = friendRequest.Id, CreatorId = user.Id, UserName = user.UserName });
                 }
             }
 
@@ -115,10 +103,13 @@ namespace Core.Services
         public async Task<ServiceResponse> SendFriendRequest(SendFriendRequest request)
         {
             Guid userId = _loggedUser.GetUserId();
-            var sentFriendRequests = await _repository.GetSentFriendRequests(userId);
-            var receivedFriendRequests = await _repository.GetReceivedFriendRequests(userId);
-            var friends = await _repository.FindByConditions(x => x.Approved && ((x.ToId == request.ToId && x.CreatorId == userId) || (x.CreatorId == request.ToId && x.ToId == userId)));
-            if(friends.Any())
+            var sentFriendRequests = await _friendRepository.GetSentFriendRequests(userId);
+            var receivedFriendRequests = await _friendRepository.GetReceivedFriendRequests(userId);
+            var friends = await _friendRepository.FindByConditions(x => x.Approved 
+            && ((x.ToId == request.ToId && x.CreatorId == userId) 
+            || (x.CreatorId == request.ToId && x.ToId == userId)));
+            
+            if (friends.Any())
             {
                 return ServiceResponse.Error("Friend has already been added.");
             }
@@ -137,7 +128,7 @@ namespace Core.Services
                 if (model.ToId == item.CreatorId)
                 {
                     item.Approved = true;
-                    await _repository.Edit(item);
+                    await _friendRepository.Edit(item);
 
                     return ServiceResponse.Success("Friend has been added.");
                 }
@@ -148,27 +139,29 @@ namespace Core.Services
                 return ServiceResponse.Error("Unable to send friend request to user with specified id.");
             }
 
-            await _repository.Create(model);
+            await _friendRepository.Create(model);
             return ServiceResponse.Success("Friend Request has been created.");
         }
 
         public async Task<ServiceResponse> RemoveFriendRequest(Guid userId)
         {
             Guid LoggedUserId = _loggedUser.GetUserId();
-            var model = await _repository.FindByConditions(fr => (fr.CreatorId == LoggedUserId && fr.ToId == userId) || (fr.CreatorId == userId && fr.ToId == LoggedUserId));
+            var model = await _friendRepository.FindByConditions(fr => 
+            (fr.CreatorId == LoggedUserId && fr.ToId == userId) 
+            || (fr.CreatorId == userId && fr.ToId == LoggedUserId));
 
             if (model.FirstOrDefault() == null)
             {
                 return ServiceResponse.Error("Entity does not exist or cannot be removed by current user.");
             }
-            await _repository.Delete(model.FirstOrDefault());
+            await _friendRepository.Delete(model.FirstOrDefault());
             return ServiceResponse.Success("Friend Request has been removed.");
         }
 
         public async Task<ServiceResponse> AcceptOrDeclineFriendRequest(ApproveFriendRequest friendRequest, int friendRequestId)
         {
             Guid userId = _loggedUser.GetUserId();
-            var model = await _repository.FindByConditions(fr => fr.Id == friendRequestId && !fr.Approved && fr.ToId == userId);
+            var model = await _friendRepository.FindByConditions(fr => fr.Id == friendRequestId && !fr.Approved && fr.ToId == userId);
             if (!model.Any())
             {
                 return ServiceResponse.Error("Not Found.");
@@ -176,26 +169,22 @@ namespace Core.Services
             var retrievedRequest = _mapper.Map<FriendRequest>(model.FirstOrDefault());
             var updateModel = _mapper.Map(friendRequest, retrievedRequest);
 
-            await _repository.Edit(updateModel);
-            if(retrievedRequest.Approved)
+            await _friendRepository.Edit(updateModel);
+            if (retrievedRequest.Approved)
             {
                 return ServiceResponse.Success("Friend has been added.");
             }
-            await _repository.Delete(retrievedRequest);
+            await _friendRepository.Delete(retrievedRequest);
             return ServiceResponse.Success("Rejected Friend Request.");
         }
 
         public async Task<bool> IsFriend(Guid? userId)
         {
             Guid LoggedUserId = _loggedUser.GetUserId();
-            var friendRequests = await _repository.GetSentAndReceivedFriendRequests(LoggedUserId);
+            var friendRequests = await _friendRepository.GetSentAndReceivedFriendRequests(LoggedUserId);
 
             var result = friendRequests.Where(fr => (fr.CreatorId == userId && fr.ToId == LoggedUserId) || (fr.CreatorId == LoggedUserId && fr.ToId == userId));
-            if (result.Any())
-            {
-                return true;
-            }
-            return false;
+            return result.Any();
         }
     }
 }
