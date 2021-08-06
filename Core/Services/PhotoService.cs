e22f20b14c3eb80cfc166203f33090e663f37dc0
Bartłomiej Wołyniec <86250990+Btwol@users.noreply.github.com>
2021-08-06 13:50:19 +0200

    #90 split the physical file upload and database save (#91)
    * #90 split the physical file upload and database save
    
    * #90 user photo upload fix

--- Core/Services/PhotoService.cs
+++ Core/Services/PhotoService.cs
@@ -77,14 +77,9 @@ namespace Core.Services
 
         public async Task<ServiceResponse> UploadPhoto(IFormFile image, string id, PhotoTypes type)
         {
-            if (!ValidateId(id, type)) return ServiceResponse.Error("Invalid Id.", HttpStatusCode.BadRequest);
-            ServiceResponse validationResult = ValidatePhoto(image);
-            if (!validationResult.SuccessStatus) return validationResult;
-            if (!(await ItemExists(id, type))) 
-                return ServiceResponse.Error("The item you're trying to upload an image for, doesn't exist.", HttpStatusCode.NotFound);
-
-            string photoPath = await ProcessPhoto(image);
-            if (photoPath == null) return ServiceResponse.Error("An error ocurred while uploading the image.");
+            var savePhotoResult = await SavePhotoToFile(image, id, type);
+            if (!savePhotoResult.SuccessStatus) return savePhotoResult;
+            string photoPath = savePhotoResult.Message;
 
             Photo photoRequest = null;
             if (type == PhotoTypes.AuthorPhoto)
@@ -92,17 +87,27 @@ namespace Core.Services
             else if (type == PhotoTypes.BookPhoto)
                 photoRequest = new BookPhoto() { BookId = int.Parse(id), Path = photoPath, PhotoType = type };
             else if (type == PhotoTypes.ProfilePhoto)
-            {
-                var idGuid = Guid.Parse(id);
-                if ((await _photoRepository.GetUserPhoto(idGuid) != null))
-                    return ServiceResponse.Error("User already has a profile photo.", HttpStatusCode.BadRequest);
-                photoRequest = new ProfilePhoto() { UserId = idGuid, Path = photoPath, PhotoType = type };
-            }
-                
+                photoRequest = new ProfilePhoto() { UserId = Guid.Parse(id), Path = photoPath, PhotoType = type };
+
             var newPhoto = _mapper.Map<PhotoDto>(await _photoRepository.UploadPhoto(photoRequest));
             return ServiceResponse<PhotoDto>.Success(newPhoto, "Photo uploaded.", HttpStatusCode.Created);
         }
-               
+              
+        private async Task<ServiceResponse> SavePhotoToFile(IFormFile image, string id, PhotoTypes type)
+        {
+            if (!ValidateId(id, type)) return ServiceResponse.Error("Invalid Id.", HttpStatusCode.BadRequest);
+            ServiceResponse validationResult = ValidatePhoto(image);
+            if (!validationResult.SuccessStatus) return validationResult;
+            if (!(await ItemExists(id, type))) return ServiceResponse
+                    .Error("The item you're trying to upload an image for, doesn't exist.", HttpStatusCode.NotFound);
+            if(type == PhotoTypes.ProfilePhoto && await _photoRepository.GetUserPhoto(Guid.Parse(id)) != null) return ServiceResponse
+                    .Error("User already has a profile photo.", HttpStatusCode.BadRequest);
+
+            string photoPath = await ProcessPhoto(image);
+            if (photoPath == null) return ServiceResponse.Error("An error ocurred while uploading the image.");
+            return ServiceResponse<string>.Success(null, photoPath);
+        }
+
         private bool ValidateId(string id, PhotoTypes type)
         {
             return (type == PhotoTypes.AuthorPhoto || type == PhotoTypes.BookPhoto) ? 
@@ -124,7 +129,6 @@ namespace Core.Services
             var extension = image.FileName.Substring(image.FileName.LastIndexOf('.'));
             if (_allowedFileExtensions.All(ex => extension != ex)) return ServiceResponse.Error("Invalid file extension", 
                 HttpStatusCode.BadRequest);
-
             return ServiceResponse.Success();
         }
 
