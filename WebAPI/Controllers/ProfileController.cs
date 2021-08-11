9444a24add6899af1d547673ea73be56ae660741
mariuszskuza02@gmail.com <mariuszskuza02@gmail.com>
2021-08-11 10:12:59 +0200

    #14 endpoints and accessibility

--- WebAPI/Controllers/ProfileController.cs
+++ WebAPI/Controllers/ProfileController.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Interfaces.Profile;
 using Core.Response;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 namespace WebAPI.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     [ApiController]
     public class ProfileController : Controller
     {
@@ -19,15 +21,15 @@ namespace WebAPI.Controllers
             _profileService = profileService;
         }
 
-        [SwaggerOperation(Summary = "Retrieves profile")]
+        [SwaggerOperation(Summary = "Retrieves profile info of specyfic user")]
         [HttpGet("Profile")]
-        public async Task<ServiceResponse> Index(Guid? id )
+        public async Task<ServiceResponse> Index(Guid? id)
         {
             return await _profileService.GetProfile(id);
         }
 
-        [SwaggerOperation(Summary = "Edits pprofile photo")]
-        [HttpPut("Profile-photo-edit")]
+        [SwaggerOperation(Summary = "Edits profile photo for current logged user")]
+        [HttpPut("Profile/EditPhoto")]
         public async Task<ServiceResponse> EditPhoto(IFormFile photo)
         {
             return await _profileService.UpdatePhoto(photo);
